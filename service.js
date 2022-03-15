35b842a324d477ef3f53fb72db315d4ed095e8ad
Hazem Khaled <hazem.khaled@gmail.com>
2022-03-15 14:32:26 +0300

    Add file upload npm

--- service.js
+++ service.js
@@ -3,11 +3,10 @@ const {
   ApolloServerPluginDrainHttpServer,
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
+const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 const {
-  ApolloGateway,
-  RemoteGraphQLDataSource,
-  IntrospectAndCompose,
-} = require("@apollo/gateway");
+  default: FileUploadDataSource,
+} = require("@profusion/apollo-federation-upload");
 const express = require("express");
 const http = require("http");
 
@@ -19,8 +18,9 @@ const gateway = new ApolloGateway({
     pollIntervalInMs,
   }),
   buildService({ url }) {
-    return new RemoteGraphQLDataSource({
+    return new FileUploadDataSource({
       url,
+      useChunkedTransfer: true,
       willSendRequest({ request, context }) {
         const headers = context.headers;
 
