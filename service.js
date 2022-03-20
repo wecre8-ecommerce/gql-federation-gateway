127d03fc3cf0af96080142f4fe111c9862048b2f
Hazem Khaled <hazem.khaled@gmail.com>
2022-03-20 13:19:00 +0300

    Lint

--- service.js
+++ service.js
@@ -46,9 +46,7 @@ const gateway = new ApolloGateway({
     gateway,
     plugins: [
       ApolloServerPluginDrainHttpServer({ httpServer }),
-      ApolloServerPluginLandingPageGraphQLPlayground({
-        httpServer: httpServer,
-      }),
+      ApolloServerPluginLandingPageGraphQLPlayground({ httpServer }),
     ],
     context({ req }) {
       return {
